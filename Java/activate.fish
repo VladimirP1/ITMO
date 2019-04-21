65f9c732c90cc522d67fa23ee7d3f3c292612e6b
Vladimir <vladimir.pinchuk01@gmail.com>
2019-04-21 23:34:16 +0300

    Simplify env setup

--- Java/activate.fish
+++
@@ -1,1 +0,0 @@
-set PATH /home/vladimir/Apps/JDK/jdk-11.0.2/bin $PATH
--- Java/import.sh
+++ Java/import.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/fish
 
 PATH=/home/vladimir/Apps/JDK/jdk-11.0.2/bin:$PATH
+GOSHA_BASE=~/University/java-advanced-2019
 
 modulesclear() {
     MODULEPATH=.
@@ -19,3 +20,7 @@ modulesload() {
         moduleload $(readlink -f "$f")
     done
 }
+
+modulesclear
+modulesload $GOSHA_BASE/lib
+modulesload $GOSHA_BASE/artifacts
