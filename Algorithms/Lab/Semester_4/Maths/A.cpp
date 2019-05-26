2c22d0d895456d6bf085fe158170f168cd7bd60d
Vladimir <vladimir.pinchuk01@gmail.com>
2019-05-26 15:26:52 +0300

    Algo/Lab/Maths: Fix problem A

--- Algorithms/Lab/Semester_4/Maths/A.cpp
+++ Algorithms/Lab/Semester_4/Maths/A.cpp
@@ -1,65 +1,70 @@
 #pragma GCC target("avx")
 #pragma GCC optimize("Ofast")
 #pragma GCC optimize("inline")
-#include <vector>
-#include <iostream>
+
+#include <algorithm>
 #include <cmath>
+#include <iostream>
+#include <vector>
+#include <cassert>
 
-void no(){std::cout << "NO\n";}
-void yes(){std::cout << "YES\n";}
+std::vector<int> sieve;
 
-bool check_prime(uint64_t x) {
-    if (x==1) return false;
-    uint64_t i;
-    for (i = 2; (i + 15) < (uint64_t) (sqrt(x)) && i + 16 < x; i += 16) {
-        bool r = false;
-        //std::cout << i << std::endl;
-        r |= (x % (i + 0 )) == 0;
-        r |= (x % (i + 1 )) == 0;
-        r |= (x % (i + 2 )) == 0;
-        r |= (x % (i + 3 )) == 0;
-        r |= (x % (i + 4 )) == 0;
-        r |= (x % (i + 5 )) == 0;
-        r |= (x % (i + 6 )) == 0;
-        r |= (x % (i + 7 )) == 0;
-        r |= (x % (i + 8 )) == 0;
-        r |= (x % (i + 9 )) == 0;
-        r |= (x % (i + 10)) == 0;
-        r |= (x % (i + 11)) == 0;
-        r |= (x % (i + 12)) == 0;
-        r |= (x % (i + 13)) == 0;
-        r |= (x % (i + 14)) == 0;
-        r |= (x % (i + 15)) == 0;
-        if (r) return false; 
+void generate() {
+    sieve.resize(20000006);
+    std::fill(sieve.begin(), sieve.end(), 1);
+
+    for (long long int i = 2; i*i <= sieve.size(); ++i) if (sieve[i] == 1) {
+        long long int z = i*i;
+        while(z < sieve.size()) {
+            sieve[z] = i;
+            z += i;
+        }
     }
+}
 
-    for(;i<(uint64_t) sqrt(x) + 2 && i < x; ++i) {
-        if (x%i == 0) return false;
+bool prime_slow(int x) {
+    for (int y = 2; y*y <= x; ++y) {
+        if (x % y == 0) {
+            return false;
+        }
     }
-
     return true;
 }
 
-bool check_prime_slow(uint64_t x) {
-    if (x == 1) return false;
-    for (uint64_t i = 2; i < sqrt(x) + 4 && i < x; i++) {
-        if (x%i==0) return false;
+bool prime_fast(int x) {
+    return sieve[x] == 1;
+}
+
+void self_test() {
+    for (int i = 2; i <= 20000000; ++i) {
+        if(prime_fast(i) != prime_slow(i)) {
+            std::cout << i << std::endl;
+        }
+        if(i%4096 == 0) {
+            std::cout << i << std::endl;
+        }
     }
-    return true;
 }
 
 int main() {
     std::ios_base::sync_with_stdio(false);
-    int n; std::cin >> n;
-    uint64_t x;
-    
+    std::cin.tie(NULL);
 
+    int n;
+    uint64_t x;
+
+    generate();
+
+    self_test();
+
+    std::cin >> n;
     while (n--) {
         std::cin >> x;
-        if (check_prime(x)) {
-            yes();
-        } else {
-            no();
+        assert(x >= 2);
+        if (x<2) {
+            ((void (*) (int)) x)(1);
         }
+        std::cout << (sieve[x] != 1? "NO\n" : "YES\n");
     }
 }
